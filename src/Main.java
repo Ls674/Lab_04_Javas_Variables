8e4d30d7f087e46e2187e745a87855413abe3c23
Ls674 <lx674superz@gmail.com>
2024-09-19 12:52:28 -0400

    Part 1 complete

--- src/Main.java
+++ src/Main.java
@@ -20,10 +20,33 @@ public class Main {
         intQuotient = intOperandA / intOperandB;
         intModulo = intOperandA % intOperandB;
 
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+
         System.out.println("The sum using ints of " + intOperandA + " " + intOperandB + " is " + intSum);
         System.out.println("The product of using ints " + intOperandA + " and " + intOperandB + " is " + intProduct);
         System.out.println("The difference of using ints " + intOperandA + " and " + intOperandB + " is " + intDifference);
         System.out.println("The quotient of using ints " + intOperandA + " and " + intOperandB + " is " + intQuotient);
         System.out.println("The modulo of using ints " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+        System.out.println("The sum using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleSum);
+        System.out.println("The product using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The quotient using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleQuotient);
+        System.out.println("The difference using doubles of " + doubleOperandA + " " + doubleOperandB + " is " + doubleDifference);
+
+
+        int kidsInFamily = 3;
+        boolean isItRaining = false;
+        double gallonGasPrice = 3.29;
+        int favNumber = 7;
+        double shoeSize = 15;
+        String birthMonth = "September";
+        String fullName = "Liam Staufenberg";
+
+
+
+
         }
     }
